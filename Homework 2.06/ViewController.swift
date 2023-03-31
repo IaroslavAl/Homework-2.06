110ea0e44d08073bdad69e9acec28779e4854662
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-03-31 15:25:09 +0400

    Initial Commit

---
+++ Homework 2.06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.06
+//
+//  Created by Iaroslav Beldin on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
