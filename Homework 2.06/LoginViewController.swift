17a020e458f54e8d6a8462f96a569118eb7fbf2a
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-03-31 17:24:10 +0400

    added logic to login screen

--- Homework 2.06/LoginViewController.swift
+++ Homework 2.06/LoginViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loginButton.layer.cornerRadius = 5
     }
-
-
+    
+    @IBAction func loginTapped() {
+        if loginTF.text == "User" && passwordTF.text == "Pass" {
+            print("Correct")
+        } else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password"
+            ) { _ in
+                self.passwordTF.text = ""
+            }
+        }
+    }
+    
+    @IBAction func forgotUserNameTapped(_ sender: UIButton) {
+        showAlert(title: "Oops!", message: "Your name is User 😉")
+    }
+    
+    @IBAction func forgotPasswordTapped(_ sender: UIButton) {
+        showAlert(title: "Oops!", message: "Your password is Pass 😉")
+    }
+    
+    private func showAlert(title: String, message: String, okActionHandler: ((UIAlertAction) -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: okActionHandler)
+        
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
-
